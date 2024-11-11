a437e380a5857e5ffcb1f06c00fcae271b7189cf
ptckxx <ptckxx@gmail.com>
2024-11-11 18:43:33 -0300

    alguns comentario e TODO's

--- c/heap-lib.c
+++ c/heap-lib.c
@@ -5,24 +5,56 @@
 
 
 /*
-* Aloca 4096 bytes na heap e aponta o ponteiro base para base da alocação
-* e o ponteiro topo para o topo da alocação
 *
+* Cria as estruturas de gerenciamento da heap
+* Aloca 4096 bytes na heap e aponta o ponteiro
+* base para base da alocação
+* e o ponteiro topo para o topo da alocação
+* TODO criar estruturas de dados para gerenciamento da heap
 */
-int iniciaAlocador(void **base, void **topo){
+void iniciaAlocador(void **base, void **topo){
     *base = sbrk(0);
     sbrk(4096);
     *topo = sbrk(0);
-    
+   
+    //TODO inicializar as duas lista, como faz isso sem malloc? boa pergunta
+    struct nodo_t blocosLivres;
+    struct nodo_t blocosOcupados;
+
+
     
     #ifdef _DEBUG_
     printf("base: %p\ntopo: %p\n",*base,*topo);
     #endif
 
-    return 0;
 }
-
+/*
+ *solicita a alocação de um bloco com num_bytes bytes
+ *na heap e que retorne o ende-reço inicial deste bloco
+ * 
+ */
 void *alocaMem(int bytes){
 
     return sbrk(bytes);
+}
+
+
+/*
+ *Libera as estruturas criadas por iniciaAlocador()
+ * 
+ */
+void finalizaAlocador(){
+
+}
+
+
+/*
+ *Devolve para a heap o bloco que foi alocado por alocaMem.
+ *O parâmetro bloco é o ende-reço retornado por alocaMem.
+ *
+ * */
+int liberaMem(void *bloco){
+
+
+    return 0;
 }
--- c/heap-lib.h
+++ c/heap-lib.h
@@ -1,6 +1,13 @@
 #include <unistd.h> //para usar brk();
 
-int iniciaAlocador(void **base, void **topo);
+struct nodo_t {
+    void *endereco;
+    int status;
+    int tam;
+    struct nodo_t *prox;
+};
+
+void iniciaAlocador(void **base, void **topo);
 
 
 void *alocaMem(int bytes);
