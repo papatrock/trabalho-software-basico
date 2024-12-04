c157fa5be60fd920cf8a2583105f850d6a621cc2
papatrock <ptckxx@gmail.com>
2024-12-03 21:43:52 -0300

    (fix) arruma bug da impressão de "." infinitos (commit bonitinho porq se deus quiser é o ultimo)

--- assembly/meuAlocador.s
+++ assembly/meuAlocador.s
@@ -228,8 +228,8 @@ bestFit:
         je true
         movq -16(%rbp), %rbx          # -16(rbp) = bestfit atual
         movq 8(%rbx),%r13
-        cmpq  %r13,8(%rax)
-        jle true
+        cmpq 8(%rax), %r13          # cmpq  %r13,8(%rax)
+        jl true
         jmp fim_cond
         true:
             movq %rax,-16(%rbp)
@@ -287,7 +287,7 @@ imprimeMapa:
     #verificar se a lista esta vazia 
     call ret_brk_atual
     movq %rax,%r15
-    
+
 
     
     cmpq $0,8(%r12)
@@ -296,7 +296,6 @@ imprimeMapa:
 
     while_inicio: 
         # comparação while
-        
         cmpq %r15,%r12
         jge while_fim
 
